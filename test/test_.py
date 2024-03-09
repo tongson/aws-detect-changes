b61792b1f571ea0df4cfeb2b582d85c2b59fb7ab
Eduardo Tongson <propolice@gmail.com>
2024-03-09 13:04:03 +0800

    Test git_status.

--- test/test_.py
+++ test/test_.py
@@ -12,6 +12,15 @@ def setup_git_init():
     shutil.rmtree(cwd)
 
 def test_git_init(setup_git_init):
+    cwd = setup_git_init
     git_init(setup_git_init)
-    config = Path(f"{setup_git_init}/.git/config")
+    config = Path(f"{cwd}/.git/config")
     assert config.is_file()
+
+def test_git_status(setup_git_init):
+    cwd = setup_git_init
+    git_init(cwd)
+    write_file = open(f"{cwd}/x", "w")
+    print(f"test", file=write_file)
+    x = git_status(cwd)
+    assert "?? x\n" == x
