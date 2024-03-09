e74b173eb24a72de9ec9424cb42dcfa95a1861fb
Eduardo Tongson <propolice@gmail.com>
2024-03-09 16:53:12 +0800

    Test git_commit and git_init should use cwd variable.

--- test/test_git.py
+++ test/test_git.py
@@ -13,7 +13,7 @@ def setup_git_init():
 
 def test_git_init(setup_git_init):
     cwd = setup_git_init
-    git_init(setup_git_init)
+    git_init(cwd)
     config = Path(f"{cwd}/.git/config")
     assert config.is_file()
 
@@ -24,3 +24,12 @@ def test_git_status(setup_git_init):
     print(f"test", file=write_file)
     x = git_status(cwd)
     assert "?? x\n" == x
+
+def test_git_commit(setup_git_init):
+    cwd = setup_git_init
+    git_init(cwd)
+    write_file = open(f"{cwd}/x", "w")
+    print(f"test", file=write_file)
+    git_commit(cwd, "xxx")
+    run = subprocess.run(["git", "log", "-1", "HEAD", "--pretty=format:%B"], cwd=cwd, text=True, check=True, capture_output=True)
+    assert "xxx\n" == run.stdout
